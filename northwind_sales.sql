802e69af1ec7b2bad788aa4de5e8915386ae8f85
SHASHI RANJAN <105940423+thesrprofile@users.noreply.github.com>
2024-05-20 12:40:22 +0530

    Update northwind_sales.sql
    northwind_sales.sql

--- northwind_sales.sql
+++ northwind_sales.sql
@@ -51,7 +51,7 @@ select first_name, last_name, max(hire_date) as hire_date from employees;
 select round(avg(unit_price),2) as unit_price, 
 sum(units_in_stock) as units_in_stock,
 sum(discontinued) as total_dict
-from products
+from products;
 
 
 -- Show the ProductName, CompanyName, CategoryName from the products, suppliers, and categories table
@@ -81,7 +81,7 @@ select City, company_name, contact_name, 'customers' as relationship
 from customers
 union
 select city, company_name, contact_name, 'suppliers'
-from suppliers
+from suppliers;
 
 
 -- Show the employee's first_name and last_name, a "num_orders" column with a count of the orders taken, and a column called "Shipped" that displays "On Time" if the order shipped_date is less or equal to the required_date, "Late" if the order shipped late.
@@ -106,7 +106,7 @@ GROUP BY
 ORDER BY
   e.last_name,
   e.first_name,
-  num_orders DESC
+  num_orders DESC;
 
 
 -- Show how much money the company lost due to giving discounts each year, order the years from most recent to least recent. Round to 2 decimal places
